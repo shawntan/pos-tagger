c5da4bf33396ddba8b58f9e323ed28b6f0659fe7
Shawn Tan <shawn@wtf.sg>
2012-09-29 02:50:41 +0800

    Counting done.

--- src/Tagger.java
+++ src/Tagger.java
@@ -1,27 +1,46 @@
-import java.io.BufferedInputStream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Hashtable;
 
 
 public class Tagger {
 	
 	public Hashtable<String, Hashtable<String,Integer>> posTransitions;
-	
-	public Tagger(BufferedInputStream bis) {
+	public Hashtable<String, Hashtable<String,Integer>> posTokCount;
+	public Tagger(BufferedReader reader) throws IOException {
 		posTransitions = new Hashtable<String, Hashtable<String,Integer>>();
+		posTokCount    = new Hashtable<String, Hashtable<String,Integer>>();
+		learn(reader);
 	}
 	
-	public void add(String w1, String w2) {
+	public void add(Hashtable<String, Hashtable<String,Integer>> table,
+					String w1, String w2) {
 		Hashtable<String,Integer> w2count;
-		if((w2count = posTransitions.get(w1)) == null) {
-			posTransitions.put(w1,w2count = new Hashtable<String,Integer>());
+		if((w2count = table.get(w1)) == null) {
+			table.put(w1,w2count = new Hashtable<String,Integer>());
 			w2count.put(w2, 0);
 		}
 		Integer c;
 		w2count.put(w2,((c = w2count.get(w2))==null?0:c)+1);
 	}
 	
-	private void learn(BufferedInputStream bis) {
-		
+	
+	
+	private void learn(BufferedReader reader) throws IOException {
+		String line;
+		while((line = reader.readLine()) != null) {
+			String[] tokens = line.split("\\s+");
+			String prevPos = null,currPos = null;
+			for(String tokPOS:tokens) {
+				String[] arrTokPos = tokPOS.split("\\/");
+				String token = arrTokPos[0];
+				currPos      = arrTokPos[arrTokPos.length-1];
+				if(prevPos != null) add(posTransitions,prevPos,currPos);
+				add(posTokCount,currPos,token.toLowerCase());
+				prevPos = currPos;
+			}
+		}
 	}
 	
 }
--- src/build_tagger.java
+++ src/build_tagger.java
@@ -1,14 +1,29 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class build_tagger {
 
 	public static void main(String[] args) {
-		Tagger t = new Tagger(null);
-		t.add("hello", "world");
-		t.add("hello", "world");
-		t.add("hello", "world");
-		t.add("hello", "world");
-		t.add("hello", "shit");
-		t.add("hello", "shit");
-		System.out.println(t.posTransitions);
+		String sentsTrain = args[0];
+		//String sentsTest  = args[2];
+		File fTrain = new File(sentsTrain);
+		//File fTest  = new File(sentsTest);
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fTrain));
+			Tagger t = new Tagger(reader);
+			reader.close();
+			System.out.println(t.posTransitions);
+			System.out.println(t.posTokCount);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
